3002c9af208af2feeb2691c40d356d97f9be2aa9
Nitin Piparava <ndpiparava@gmail.com>
2025-07-07 09:24:33 +0200

    CAMP-000 feat: project setup

---
+++ android/app/src/main/kotlin/com/example/flutter_campsite_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_campsite_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'views/home_view.dart';
+
+void main() {
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Campsites',
+      theme: ThemeData(primarySwatch: Colors.green),
+      // home: HomeView(),
+    );
+  }
+}
